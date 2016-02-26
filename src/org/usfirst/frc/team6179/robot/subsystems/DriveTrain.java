6f910ece6f5e311c255eded7161f12f0f071cb14
Smigus <Smigum@outlook.com>
2016-02-26 20:49:41 +0800

    Fixed some dudu in code concerning driveTrain.

--- src/org/usfirst/frc/team6179/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team6179/robot/subsystems/DriveTrain.java
@@ -3,30 +3,49 @@ package org.usfirst.frc.team6179.robot.subsystems;
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team6179.robot.LogitechGamepad;
+import org.usfirst.frc.team6179.robot.OI;
 
 /**
  *
  */
 public class DriveTrain extends Subsystem {
 
+    TalonSRX leftMotor;
+    TalonSRX rightMotor;
+
+    // Multiplies the speed to be set when setting the speed
     public double speedMultiplier = 0.8;
+
+    // Multiplies the turning speed to be set when setting the turning speed
     public double turnMultiplier = 0.6;
+
+    // Indicates whether the speed input should be squared
+    public boolean squaredInput = false;
 
     private RobotDrive drive;
 
     public DriveTrain(int left, int right) {
-        TalonSRX leftMotor = new TalonSRX(left);
-        TalonSRX rightMotor = new TalonSRX(right);
+        leftMotor = new TalonSRX(left);
+        rightMotor = new TalonSRX(right);
         drive = new RobotDrive(leftMotor, rightMotor);
     }
 
-    public DriveTrain(int motor, double speed) {
-        TalonSRX Motor = new TalonSRX(motor);
-        Motor.set(speed);
+//    public DriveTrain(int motor, double speed) {
+//        TalonSRX Motor = new TalonSRX(motor);
+//        Motor.set(speed);
+//    }
+    
+    public void arcadeDrive(double movement, double rotation) {
+        drive.arcadeDrive(movement * speedMultiplier, rotation * turnMultiplier, squaredInput);
     }
 
-    public void arcadeDrive(double movement, double rotation) {
-        drive.arcadeDrive(movement * speedMultiplier, rotation * turnMultiplier);
+    public void tankDrive(double leftMovement, double rightMovement) {
+        drive.tankDrive(leftMovement * speedMultiplier, rightMovement * speedMultiplier, squaredInput);
+    }
+
+    public void stop() {
+        drive.arcadeDrive(0, 0);
     }
 
     public void initDefaultCommand() {
