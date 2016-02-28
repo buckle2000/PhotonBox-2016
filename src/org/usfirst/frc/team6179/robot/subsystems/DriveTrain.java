8b38511c6b4ee0278775e58397e31d05a1b15e0d
Smigus <Smigum@outlook.com>
2016-02-28 13:34:42 +0800

    Add test for gyro.

--- src/org/usfirst/frc/team6179/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team6179/robot/subsystems/DriveTrain.java
@@ -3,11 +3,13 @@ package org.usfirst.frc.team6179.robot.subsystems;
 
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team6179.robot.commands.drivetrain.ArcadeDriveWithJoystick;
 import org.usfirst.frc.team6179.robot.commands.shooter.ShootBoulder;
 import org.usfirst.frc.team6179.robot.configurations.DriveTrainConfig;
 import org.usfirst.frc.team6179.robot.mappings.RobotMap;
 import org.usfirst.frc.team6179.robot.sensors.BMA220Accelerometer;
+import org.usfirst.frc.team6179.robot.sensors.GY521GyroAccelerometer;
 
 /**
  *
@@ -23,13 +25,15 @@ public class DriveTrain extends Subsystem {
     // Indicates whether the speed input should be squared
     public boolean squaredInput = false;
 
-    private BMA220Accelerometer accelerometer;
+//    private BMA220Accelerometer accelerometer;
+    public GY521GyroAccelerometer gy521;
 
     private RobotDrive drive;
 
     public DriveTrain() {
         drive = new RobotDrive(RobotMap.leftMotor, RobotMap.rightMotor);
-        accelerometer = new BMA220Accelerometer();
+        gy521 = new GY521GyroAccelerometer();
+//        accelerometer = new BMA220Accelerometer();
     }
 
     public void arcadeDrive(double movement, double rotation) {
@@ -45,20 +49,24 @@ public class DriveTrain extends Subsystem {
     }
 
     /** Gets the forward acceleration of the robot in g-force */
-    public double getForwardAcceleration() {
-        return accelerometer.getY();
-    }
+//    public double getForwardAcceleration() {
+//        return accelerometer.getY();
+//    }
 
-    /**
-     * Gets the angular acceleration of the robot in g-force.
-     * Positive values for clockwise accelerations, negative values for counterclockwise accelerations.
-     */
-    public double getAngularAcceleration() {
-        double aX = accelerometer.getX();
-        double deltaX = DriveTrainConfig.accelerometerOffsetX;
-        double deltaY = DriveTrainConfig.accelerometerOffsetY;
+//    /**
+//     * Gets the angular acceleration of the robot in g-force.
+//     * Positive values for clockwise accelerations, negative values for counterclockwise accelerations.
+//     */
+//    public double getAngularAcceleration() {
+//        double aX = accelerometer.getX();
+//        double deltaX = DriveTrainConfig.accelerometerOffsetX;
+//        double deltaY = DriveTrainConfig.accelerometerOffsetY;
+//
+//        return aX * deltaX + aX * deltaX * deltaX / deltaY;
+//    }
 
-        return aX * deltaX + aX * deltaX * deltaX / deltaY;
+    public double getAngularVelocity() {
+        return gy521.getGyroZ();
     }
 
     public void initDefaultCommand() {
