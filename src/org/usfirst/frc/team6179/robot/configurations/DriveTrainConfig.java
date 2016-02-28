d8fe79186e69e23dbe73574afbe7e05857ccc9f6
Smigus <Smigum@outlook.com>
2016-02-28 13:22:38 +0800

    Add gyro.

--- src/org/usfirst/frc/team6179/robot/configurations/DriveTrainConfig.java
+++ src/org/usfirst/frc/team6179/robot/configurations/DriveTrainConfig.java
@@ -5,6 +5,16 @@ package org.usfirst.frc.team6179.robot.configurations;
  */
 public class DriveTrainConfig {
 
+    /**
+     * The offset of the accelerometer to the robot's center of rotation along X-axis.
+     * Positive when the accelerometer is to the right of the center of rotation.
+     */
     public static double accelerometerOffsetX = 0;
+    /**
+     * The offset of the accelerometer to the robot's center of rotation along Y-axis.
+     * Positive when the accelerometer is in front of the center of rotation.
+     */
     public static double accelerometerOffsetY = 42;
+
+    public static double gyroSensitivity = 131.00;
 }
