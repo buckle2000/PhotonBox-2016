248e804c7a28246d46ba653e54031ed0bf461a09
Caroline <2231948903@qq.com>
2016-02-29 19:32:51 +0800

    updated once

--- src/org/usfirst/frc/team6179/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team6179/robot/subsystems/DriveTrain.java
@@ -2,9 +2,11 @@
 package org.usfirst.frc.team6179.robot.subsystems;
 
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import org.usfirst.frc.team6179.robot.commands.drivetrain.ArcadeDriveWithJoystick;
 import org.usfirst.frc.team6179.robot.mappings.RobotMap;
+import org.usfirst.frc.team6179.robot.sensors.GY521GyroAccelerometer;
 
 /**
  *
@@ -20,10 +22,32 @@ public class DriveTrain extends Subsystem {
     // Indicates whether the speed input should be squared
     public boolean squaredInput = false;
 
+    public double angle;
+
     private RobotDrive drive;
+
 
     public DriveTrain() {
         drive = new RobotDrive(RobotMap.leftMotor, RobotMap.rightMotor);
+=======
+    public boolean squaredInput = true;
+
+    // private BMA220Accelerometer accelerometer;
+    public GY521GyroAccelerometer gy521;
+
+    public double angle;
+
+    private RobotDrive drive;
+
+    private Timer timer;
+
+    public DriveTrain() {
+        drive = new RobotDrive(RobotMap.leftMotor, RobotMap.rightMotor);
+        timer = new Timer();
+        gy521 = new GY521GyroAccelerometer();
+        timer.start();
+//        accelerometer = new BMA220Accelerometer();
+>>>>>>> Stashed changes
     }
 
     public void arcadeDrive(double movement, double rotation) {
@@ -38,6 +62,16 @@ public class DriveTrain extends Subsystem {
         drive.arcadeDrive(0, 0);
     }
 
+<<<<<<< Updated upstream
+=======
+    public double getAngularVelocity() {
+        double angularVelocity = gy521.getGyroZ();
+        angle += timer.get() * angularVelocity;
+        timer.reset();
+        return gy521.getGyroZ();
+    }
+
+>>>>>>> Stashed changes
     public void initDefaultCommand() {
         setDefaultCommand(new ArcadeDriveWithJoystick());
     }
