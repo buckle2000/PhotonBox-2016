dc52fc02a8073397885cd93c5bcb3bde65ac6e01
Smigus <zhengcheng.huang@outlook.com>
2016-03-03 15:36:32 +0800

    Reset Climber Lock when initializing `Climber`

--- src/org/usfirst/frc/team6179/robot/subsystems/Climber.java
+++ src/org/usfirst/frc/team6179/robot/subsystems/Climber.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import org.usfirst.frc.team6179.robot.configurations.ClimberConfig;
+import org.usfirst.frc.team6179.robot.configurations.ShooterConfig;
 import org.usfirst.frc.team6179.robot.mappings.RobotMap;
 
 public class Climber extends Subsystem {
@@ -18,6 +20,7 @@ public class Climber extends Subsystem {
     public Encoder climberMotorEncoder = new Encoder(RobotMap.channelA, RobotMap.channelB, true, Encoder.EncodingType.k4X);
 
     public Climber() {
+        lockServo.set(ClimberConfig.lockServoLockPosition);
         LiveWindow.addActuator("Climber", "Lock Servo", lockServo);
         LiveWindow.addActuator("Wrist", "GearMotor", climberMotor);
         LiveWindow.addSensor("Wrist", "Encoder", climberMotorEncoder);
