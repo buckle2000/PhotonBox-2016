edb6db663f2a46df25e832c464e7bfd2ab1c1973
CC <dsy1998@live.com>
2016-03-05 17:50:16 +0800

    更新射球机构的一些设置

--- src/org/usfirst/frc/team6179/robot/configurations/ShooterConfig.java
+++ src/org/usfirst/frc/team6179/robot/configurations/ShooterConfig.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team6179.robot.configurations;
 public class ShooterConfig {
 
     public static double triggerServoBackPosition = 0.3;
-    public static double triggerServoForwardPosition = 0.8;
+    public static double triggerServoForwardPosition = 0.9;
 
     /**
      * the delay, in seconds, from when the shooter motors start and when the trigger delivers the boulder.
@@ -17,6 +17,5 @@ public class ShooterConfig {
     /** The output signal to send to the shooter motors to make the boulder go outward. */
     public static double outgoingOutput = -1;
     /** The output signal to send to the shooter motors to make the boulder go inward. */
-    public static double ingoingOutput = 0.6;
 
 }
